33bcf12f9ddb8a7d096b043c03ac64c8b6f34479
Giap-Tuyen9806 <88347329+Giap-Tuyen9806@users.noreply.github.com>
2021-08-13 18:14:39 +0700

    Add files via upload

---
+++ tinhhinhtamgiac.py
@@ -0,0 +1,7 @@
+import math
+a = float(input("Nhập vào chiều dài a: "))
+b = float(input("Nhập vào chiều dài b: "))
+c = float(input("Nhập vào chiều dài c: "))
+s = (a+b+c)/2
+area = math.sprt(s*(s-a)*(s-b)*(s-c))
+print("Diện tích của tam giác: ","diện tíc")
